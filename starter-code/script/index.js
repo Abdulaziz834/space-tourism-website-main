2e821b80eda80088c40ed0ca76f5a4e55956448f
Abdulaziz <abdullaevabdulaziz123@gmail.com>
2023-06-27 22:00:42 +0500

    Final work in index

--- starter-code/script/index.js
+++ starter-code/script/index.js
@@ -1,5 +1,31 @@
-// const navBtn = document.querySelector("button.nav-open");
+const ulNav = document.querySelector("ul.navigation"),
+	navBtn = document.querySelector("button.nav-open.mobile"),
+	closeBtn = document.querySelector(".section-close.mobile > .btn-close");
 
-// navBtn.onclick = e => {
-// 	e.target.parentElement.classList.toggle("opened")
-// }
+navBtn.onclick = e => {
+	ulNav.style.display = "flex";
+	ulNav.setAttribute("opening", "");
+	ulNav.addEventListener("animationend", e => {
+		ulNav.classList.add("opened");
+		ulNav.removeAttribute("opening");
+	}, {once: true})
+
+}
+
+closeBtn.onclick = e => {
+	ulNav.setAttribute("closing", "");
+	ulNav.addEventListener("animationend", e => {
+		ulNav.classList.remove("opened");
+		ulNav.removeAttribute("closing");
+		ulNav.style.display = "none";
+	}, {once: true})
+}
+
+window.onresize = e => {
+	if (window.innerWidth > 650) {
+		ulNav.style.display = "flex";
+	}
+	else {
+		ulNav.style.display = "none";
+	}
+}
